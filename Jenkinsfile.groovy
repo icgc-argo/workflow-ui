31158d1e2b454c7a5ebcac3de170b1f6fffd7a67
Minh <hlminh2000@gmail.com>
2019-12-11 13:54:44 -0500

    :hankey: fixing it again!!!

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -36,7 +36,7 @@ spec:
                     commit = sh(returnStdout: true, script: 'git describe --always').trim()
                 }
                 script {
-                    version = sh(returnStdout: true, script: 'cat ./package.json | grep \"version\"  | cut -d \':\' -f2 | sed -e \'s/"//\' -e \'s/",//\'').trim()
+                    version = sh(returnStdout: true, script: 'cat ./package.json | grep "version" | cut -d \':\' -f2 | sed -e \'s/"//\' -e \'s/",//\'').trim()
                 }
                 script {
                     sh "echo ${commit}"
