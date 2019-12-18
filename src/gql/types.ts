f6a90a1c3ce5bda1dcd6153a41cc5fbe642e13bd
Alex Lepsa <lepsalex@gmail.com>
2019-12-18 13:22:31 -0500

    RunRequest workflow_params type update

--- src/gql/types.ts
+++ src/gql/types.ts
@@ -40,7 +40,7 @@ export type RunDetail = {
 };
 
 export type RunRequest = {
-  workflow_params: {};
+  workflow_params: {[k: string]: any};
   workflow_type: string;
   workflow_type_version: string;
   workflow_url: string;
