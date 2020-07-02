8150ca08c2190b388c8012dd04b074a821d60800
Alex <lepsalex@gmail.com>
2020-07-02 11:39:54 -0400

    FIX: MomentJS Invalid Date message

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
 import moment from 'moment-timezone';
 
+moment.updateLocale('en', {
+    invalidDate: "N/A"
+})
+
 export const parseEpochToEST = (milli: string) => moment(parseInt(milli)).tz('America/Toronto').format('MMMM Do YYYY, h:mm:ss a')
