87c74115c5eb2410d8ea3670734785e5d96e5626
Yelizar Alturmessov <yalturmessov@oicr.on.ca>
2020-07-07 16:20:36 -0400

    Prep dev deploy stage for merge into develop branch. Add qa deploy stage for master branch

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -73,8 +73,7 @@ spec:
 
         stage('deploy to rdpc-collab-dev') {
             when {
-// Change branch to develop after successful testing
-                branch "2020-07-07-add-jenkins-deploy-stage"
+                branch "develop"
             }
             steps {
                 build(job: "/provision/helm", parameters: [
@@ -106,6 +105,22 @@ spec:
                 }
             }
         }
+
+        stage('deploy to rdpc-collab-qa') {
+            when {
+                branch "master"
+            }
+            steps {
+                build(job: "/provision/helm", parameters: [
+                    [$class: 'StringParameterValue', name: 'AP_RDPC_ENV', value: 'qa' ],
+                    [$class: 'StringParameterValue', name: 'AP_CHART_NAME', value: 'workflow-ui'],
+                    [$class: 'StringParameterValue', name: 'AP_RELEASE_NAME', value: 'ui'],
+                    [$class: 'StringParameterValue', name: 'AP_HELM_CHART_VERSION', value: "${chartVersion}"],
+                    [$class: 'StringParameterValue', name: 'AP_ARGS_LINE', value: "--set-string image.tag=${version}" ]
+                ])
+            }
+        }
+
     }
     post {
         unsuccessful {
