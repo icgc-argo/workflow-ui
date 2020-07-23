faa2e02e0446cfc8a0228f8ae405d2edaf734668
Alex <lepsalex@gmail.com>
2020-07-23 13:32:18 -0400

    cancel button implemented ... one day we will have gql subscriptions

--- src/components/NewRunFormModal.tsx
+++ src/components/NewRunFormModal.tsx
@@ -17,7 +17,6 @@
  */
 
 import React from "react";
-import urlJoin from "url-join";
 import Button from "@icgc-argo/uikit/Button";
 import { ModalPortal } from "../App";
 import Modal from "@icgc-argo/uikit/Modal";
@@ -29,8 +28,7 @@ import "ace-builds/src-noconflict/theme-github";
 import { css } from "emotion";
 import { ApolloError } from "apollo-client";
 import { useTheme } from "@icgc-argo/uikit/ThemeProvider";
-
-const MANAGEMENT_API = process.env.REACT_APP_MANAGEMENT_API || "";
+import { runWorkflow } from "../rdpc";
 
 type RunSuccess = {
   run_id: string;
@@ -56,25 +54,6 @@ export default ({
     RunResponse | ApolloError | undefined | null
   >(null);
   const theme = useTheme();
-
-  const runWorkflow = ({
-    workflow_url,
-    workflow_params,
-    workflow_engine_params,
-  }: {
-    workflow_url: string;
-    workflow_params: string;
-    workflow_engine_params: string;
-  }): Promise<{ run_id: string }> =>
-    fetch(urlJoin(MANAGEMENT_API, "/runs"), {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        workflow_url,
-        workflow_params,
-        workflow_engine_params,
-      }),
-    }).then(async (res) => res.json());
 
   const onNewRunClick: React.ComponentProps<typeof Button>["onClick"] = () => {
     setNewRunModalShown(true);
